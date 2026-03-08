594e1e58242bfb2b9ffec0f4d32edced250e20c9
darzizalol <darzizalol@gmail.com>
2026-03-08 21:55:11 +0800

    abstracting say_hello fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
+
+fn say_hello() -> &'static str {
+    "Hello world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            say_hello()
+        }
+    });
+
+    server.listen("127.0.0.1:6767").await.unwrap();
 }
